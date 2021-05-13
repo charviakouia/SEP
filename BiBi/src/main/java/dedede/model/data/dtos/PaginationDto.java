b8cd38b9c8a90c2e45a7cf5a7cfc20df7cb9f251
najjar <najjar@fim.uni-passau.de>
2021-05-13 23:14:07 +0200

    PaginationDto added

--- BiBi/src/main/java/dedede/model/data/dtos/PaginationDto.java
+++ BiBi/src/main/java/dedede/model/data/dtos/PaginationDto.java
@@ -1,4 +1,61 @@
 package dedede.model.data.dtos;
 
-public class PaginationDto {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaginationDto implements Serializable {
+
+    private static final  long serialVersionID = 1L;
+
+    private final Map<String,String> filterCriteria = new HashMap<>();
+
+    private int pageSize;
+
+    private int page;
+
+    private int totalAmountOfRows;
+
+    private String sortBy;
+
+    public PaginationDto() {
+
+    }
+
+    public void nextPage() {
+
+    }
+
+    public void setPage(int page) {
+
+    }
+
+    public void previousPage() {
+
+
+    }
+
+    public int getCurrentPage() {
+        return page;
+    }
+
+    public void setSortBy(String sortBy) {
+
+    }
+
+    public String getSortBy() {
+        return sortBy;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
