894c3c5e632ab1b9a4e7d4eecef5850f1a3f2ce0
najjar <najjar@fim.uni-passau.de>
2021-05-20 18:04:17 +0200

    @FacesConverter added for all converters

--- BiBi/src/main/java/de/dedede/model/logic/converters/SearchOperatorConverter.java
+++ BiBi/src/main/java/de/dedede/model/logic/converters/SearchOperatorConverter.java
@@ -2,10 +2,12 @@ package de.dedede.model.logic.converters;
 
 import de.dedede.model.logic.util.SearchOperator;
 import jakarta.faces.convert.EnumConverter;
+import jakarta.faces.convert.FacesConverter;
 
 /**
  * Checks if the input for this search field is a legal.
  */
+@FacesConverter(forClass = SearchOperator.class)
 public class SearchOperatorConverter extends EnumConverter {
 
 	public SearchOperatorConverter() {
