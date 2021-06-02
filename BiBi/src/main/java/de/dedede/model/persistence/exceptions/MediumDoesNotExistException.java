44313f57fc08bb35712593474807ed4323fd8119
Sergei Pravdin <sergpravdin@icloud.com>
2021-06-02 17:24:24 +0200

    Exceptions

--- BiBi/src/main/java/de/dedede/model/persistence/exceptions/MediumDoesNotExistException.java
+++ BiBi/src/main/java/de/dedede/model/persistence/exceptions/MediumDoesNotExistException.java
@@ -1,41 +1,41 @@
 package de.dedede.model.persistence.exceptions;
 
 /**
- * This checked exception describes situations in which an entity, as represented
+ * This checked exception describes situations in which a medium, as represented
  * by a data entry, is absent from the data store. This generally means that
  * an attempt to reference an entity by an ID was carried out with an ID that
  * doesn't refer to anything. Sometimes, this may be the result of a race-condition.
  */
-public class EntityInstanceDoesNotExistException extends Exception {
+public class MediumDoesNotExistException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
     /**
-     * Creates a new EntityInstanceDoesNotExistException instance.
+     * Creates a new MediumDoesNotExistException instance.
      */
-    public EntityInstanceDoesNotExistException() {
+    public MediumDoesNotExistException() {
         super();
     }
 
     /**
-     * Creates a new EntityInstanceDoesNotExistException instance with a
+     * Creates a new MediumDoesNotExistException instance with a
      * display message identifying the specific cause.
      *
      * @param msg The message indicating the exception's cause.
      */
-    public EntityInstanceDoesNotExistException(String msg) {
+    public MediumDoesNotExistException(String msg) {
         super(msg);
     }
 
     /**
-     * Creates a new EntityInstanceDoesNotExistException instance with a
+     * Creates a new MediumDoesNotExistException instance with a
      * display message identifying the specific cause.
      *
-     * @param msg   The message indicating the exception's cause.
+     * @param msg    The message indicating the exception's cause.
      * @param cause the cause (which is saved for later retrieval by the Throwable.getCause() method).
-     *              (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+     *               (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
      */
-    public EntityInstanceDoesNotExistException(String msg, Throwable cause) {
+    public MediumDoesNotExistException(String msg, Throwable cause) {
         super(msg, cause);
     }
 
