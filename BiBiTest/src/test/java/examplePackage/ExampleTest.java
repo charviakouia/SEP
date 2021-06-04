af68d9105d341ee7c3e79da92e298e5cbd394991
Ivan Charviakou <ivancharviakou@gmail.com>
2021-06-04 11:29:56 +0200

    Added test module to project

---
+++ BiBiTest/src/test/java/examplePackage/ExampleTest.java
@@ -0,0 +1,14 @@
+package examplePackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
