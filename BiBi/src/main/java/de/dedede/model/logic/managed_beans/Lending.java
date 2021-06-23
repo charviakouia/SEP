547893ae1e959a206d924d1280bfb693faa844e2
León Liehr <liehr@fim.uni-passau.de>
2021-06-23 14:22:18 +0200

    Merge branch 'secluded' into master

--- BiBi/src/main/java/de/dedede/model/logic/managed_beans/Lending.java
+++ BiBi/src/main/java/de/dedede/model/logic/managed_beans/Lending.java
@@ -36,27 +36,27 @@ public class Lending implements Serializable {
 
 	@Serial
 	private static final long serialVersionUID = 1;
-	
+
 	/**
-	 * Recieves the email address of the top input field on value change 
+	 * Recieves the email address of the top input field on value change
 	 */
 	private UserDto user = new UserDto();
-	
+
 	/**
 	 * Reflects the input fields for signatures as an ArrayList
 	 */
-	private List<CopyDto> copies = new ArrayList<CopyDto>(); //normale list
+	private List<CopyDto> copies = new ArrayList<CopyDto>(); // normale list
 
 	/**
 	 * Initializes the Backing Bean with 5 copy signature input fields.
 	 */
 	@PostConstruct
 	public void init() {
-		for(int i = 0; i < 5; i++) { 							//feld extrahieren
+		for (int i = 0; i < 5; i++) { // feld extrahieren
 			copies.add(new CopyDto());
 		}
 	}
-	
+
 	/**
 	 * Lend the list of copies with existing signatures to the existing user.
 	 * 
@@ -64,67 +64,53 @@ public class Lending implements Serializable {
 	 */
 	public void lendCopies() {
 		int lent = 0;
-		for(CopyDto copy : copies) {
-			if (copy.getSignature() != null 
-					&& copy.getSignature().trim() != "") {
+		for (CopyDto copy : copies) {
+			if (copy.getSignature() != null && copy.getSignature().trim() != "") {
 				try {
 					MediumDao.lendCopy(copy, user);
 					lent++;
 				} catch (CopyDoesNotExistException e) {
-					String message = "An unexpected error occured during "
-							+ "lending process, the copy didn't exist.";
+					String message = "An unexpected error occured during " + "lending process, the copy didn't exist.";
 					Logger.severe(message);
 					throw new BusinessException(message, e);
 				} catch (UserDoesNotExistException e) {
 					String message = "An unexpected error occured during "
-							+ "lending process, the user wasn't found in "
-							+ "the database.";
+							+ "lending process, the user wasn't found in " + "the database.";
 					Logger.severe(message);
 					throw new BusinessException(message, e);
 				} catch (CopyIsNotAvailableException e) {
 					String message = "An unexpected error occured during "
-							+ "lending process, the copy is not available "
-							+ "for lending.";
+							+ "lending process, the copy is not available " + "for lending.";
 					Logger.severe(message);
 					throw new BusinessException(message, e);
 				} catch (InvalidUserForCopyException e) {
 					String message = "An unexpected error occured during "
-							+ "lending process, the copy wasn't marked for "
-							+ "pickup by this user.";
+							+ "lending process, the copy wasn't marked for " + "pickup by this user.";
 					Logger.severe(message);
 					throw new BusinessException(message, e);
 				}
 			}
 		}
 		FacesContext context = FacesContext.getCurrentInstance();
-			Application application = context.getApplication();
-			ResourceBundle messages = application.evaluateExpressionGet(context,
-					"#{msg}", ResourceBundle.class);
-				if (lent == 0) {
-					String shortMessage = messages.getString("lending.enter_"
-							+ "signature_short");
-					String longMessage = messages.getString("lending.enter_"
-							+ "signature_long");
-					context.addMessage(null, new FacesMessage(
-					FacesMessage.SEVERITY_ERROR, shortMessage, longMessage));
-				} else {
-					String shortContent = messages.getString("lending.copies"
-							+ "_lent_short");
-					String longContent = messages.getString("lending.copies_"
-							+ "lent_long");
-					String emailAddress = user.getEmailAddress();
-					String lentCopies = String.valueOf(lent);
-					String shortMessage = insertParams(lentCopies, emailAddress, 
-					shortContent);
-					String longMessage = insertParams(lentCopies, emailAddress, 
-					longContent);
-					context.addMessage(null, new FacesMessage(
-					FacesMessage.SEVERITY_INFO, shortMessage, longMessage));
-					this.user.setEmailAddress("");
-					this.copies.clear();
+		Application application = context.getApplication();
+		ResourceBundle messages = application.evaluateExpressionGet(context, "#{msg}", ResourceBundle.class);
+		if (lent == 0) {
+			String shortMessage = messages.getString("lending.enter_" + "signature_short");
+			String longMessage = messages.getString("lending.enter_" + "signature_long");
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, shortMessage, longMessage));
+		} else {
+			String shortContent = messages.getString("lending.copies" + "_lent_short");
+			String longContent = messages.getString("lending.copies_" + "lent_long");
+			String emailAddress = user.getEmailAddress();
+			String lentCopies = String.valueOf(lent);
+			String shortMessage = insertParams(lentCopies, emailAddress, shortContent);
+			String longMessage = insertParams(lentCopies, emailAddress, longContent);
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, shortMessage, longMessage));
+			this.user.setEmailAddress("");
+			this.copies.clear();
 		}
 	}
-	
+
 	/**
 	 * Action for a listener on the user email input field.
 	 * 
@@ -140,14 +126,14 @@ public class Lending implements Serializable {
 	public void addSignatureInputField() {
 		copies.add(new CopyDto());
 	}
-	
+
 	/**
 	 * Grants the facelet access to the user input container.
 	 * 
 	 * @return the input container.
 	 */
 	public UserDto getUser() {
-		
+
 		return user;
 	}
 
@@ -161,13 +147,13 @@ public class Lending implements Serializable {
 	}
 
 	/**
-	 * Allows the corresponding facelet to access the variable number of 
-	 * CopyDtos that mirror the signature input fields.
+	 * Allows the corresponding facelet to access the variable number of CopyDtos
+	 * that mirror the signature input fields.
 	 * 
 	 * @return the list of CopyDtos.
 	 */
 	public List<CopyDto> getCopies() {
-		
+
 		return copies;
 	}
 
@@ -176,15 +162,15 @@ public class Lending implements Serializable {
 	 * 
 	 * @param copies the list of copyDtos that hold the data.
 	 */
-	public void setCopies(ArrayList<CopyDto> copies) {
+	public void setCopies(List<CopyDto> copies) {
 		this.copies = copies;
 	}
 
 	private String insertParams(String param1, String param2, String content) {
 		MessageFormat messageFormat = new MessageFormat(content);
-		Object[] args = {param1, param2};
+		Object[] args = { param1, param2 };
 		String contentWithParam = messageFormat.format(args);
-		
+
 		return contentWithParam;
 	}
 
