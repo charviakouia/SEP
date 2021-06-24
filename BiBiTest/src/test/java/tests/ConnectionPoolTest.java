122438027ee4861625a038e30daee41d4ad735e6
Ivan Charviakou <ivancharviakou@gmail.com>
2021-06-24 18:11:14 +0200

    Documented tests

--- BiBiTest/src/test/java/tests/ConnectionPoolTest.java
+++ BiBiTest/src/test/java/tests/ConnectionPoolTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.sql.Connection;
@@ -7,38 +8,45 @@ import java.sql.SQLException;
 import java.time.Duration;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Properties;
 
-import de.dedede.model.logic.util.RegisteredUserLendStatus;
-import de.dedede.model.logic.util.SystemAnonAccess;
-import de.dedede.model.logic.util.SystemRegistrationStatus;
-import de.dedede.model.persistence.exceptions.InvalidConfigurationException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import de.dedede.model.logic.util.RegisteredUserLendStatus;
+import de.dedede.model.logic.util.SystemAnonAccess;
+import de.dedede.model.logic.util.SystemRegistrationStatus;
+import de.dedede.model.persistence.exceptions.InvalidConfigurationException;
 import de.dedede.model.data.dtos.ApplicationDto;
 import de.dedede.model.persistence.daos.ApplicationDao;
 import de.dedede.model.persistence.exceptions.EntityInstanceDoesNotExistException;
 import de.dedede.model.persistence.exceptions.LostConnectionException;
 import de.dedede.model.persistence.exceptions.MaxConnectionsException;
-import de.dedede.model.persistence.util.ConfigReader;
 import de.dedede.model.persistence.util.ConnectionPool;
 
+/**
+ * Tests the functionality of the connection pool. In particular, connections are retrieved, returned, and used
+ * to conduct basic operations in the data store. In addition, the pool's capacity is tested by depleting it of all
+ * connections.
+ *
+ * @author Ivan Charviakou
+ */
 class ConnectionPoolTest {
 
-	private static ConnectionPool poolInstance;
 	private static final long CONNECTION_TIMEOUT = 5000;
+
 	private static ApplicationDto dto;
+	private static int capacity;
+
 	private boolean resetPool = false;
 	private List<Connection> connections = new LinkedList<>();
 	
 	@BeforeAll
 	public static void setUp() throws ClassNotFoundException, SQLException, InvalidConfigurationException {
 		PreTest.setUp();
-		poolInstance = ConnectionPool.getInstance();
 		initializeDto();
+		capacity = Integer.parseInt(PreTest.getProperty("DB_CAPACITY"));
 	}
 	
 	@AfterAll
@@ -50,30 +58,51 @@ class ConnectionPoolTest {
 	public void resetPool() {
 		if (resetPool) {
 			for (Connection conn : connections) {
-				poolInstance.releaseConnection(conn);
+				ConnectionPool.getInstance().releaseConnection(conn);
 			}
 		}
 		resetPool = false;
 	}
-	
+
+	/**
+	 * Ensures that the connection pool reacts correctly to being depleted of all its connections.
+	 */
 	@Test
 	public void testExhaustingConnectionPool() {
 		resetPool = true;
-		assertThrows(MaxConnectionsException.class, () -> exhaustConnectionPool());
+		assertDoesNotThrow(this::exhaustConnectionPool);
+		ConnectionPool pool = ConnectionPool.getInstance();
+		assertThrows(MaxConnectionsException.class, () -> pool.fetchConnection(CONNECTION_TIMEOUT));
 	}
 	
 	private void exhaustConnectionPool() {
-		while (true) {
-			connections.add(poolInstance.fetchConnection(CONNECTION_TIMEOUT));
+		for (int i = 0; i < capacity; i++){
+			Connection conn = ConnectionPool.getInstance().fetchConnection(CONNECTION_TIMEOUT);
+			connections.add(conn);
 		}
 	}
-	
+
+	/**
+	 * Tests whether connection retrieval and return work as expected.
+	 *
+	 * @throws MaxConnectionsException	Is thrown when no connection is currently available
+	 */
 	@Test
 	public void testGettingAndReturningConnection() throws MaxConnectionsException {
-		Connection conn = poolInstance.fetchConnection(CONNECTION_TIMEOUT);
-		poolInstance.releaseConnection(conn);
+		Connection conn = ConnectionPool.getInstance().fetchConnection(CONNECTION_TIMEOUT);
+		ConnectionPool.getInstance().releaseConnection(conn);
 	}
-	
+
+	/**
+	 * Ensures that the returned connections are valid and can be used to perform operations in the datastore.
+	 *
+	 * @throws MaxConnectionsException				Is thrown when no connection is available to carry out the
+	 * 												operation
+	 * @throws LostConnectionException				Is thrown when the used connection stopped working correctly
+	 * 												before completing the transaction.
+	 * @throws EntityInstanceDoesNotExistException	Is thrown if the queried id isn't present in the datastore,
+	 * 												typically the result of a race-condition
+	 */
 	@Test
 	public void testDataCreationAndDeletion() throws MaxConnectionsException, LostConnectionException,
 			EntityInstanceDoesNotExistException {
