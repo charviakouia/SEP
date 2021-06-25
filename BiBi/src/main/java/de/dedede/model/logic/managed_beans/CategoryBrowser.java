ce6f0960b1f5263f0acd6dc4a37143ed5c210589
León Liehr <liehr@fim.uni-passau.de>
2021-06-25 21:06:12 +0200

    Make the leaves command links
    But now, we cannot create the tree before rendering anymore since we
    need an ELContext. That's why I temporarily introduced a button to
    construct the tree. I hope that later I can replace it with some kind of
    event listener (postRenderView or sth)

--- BiBi/src/main/java/de/dedede/model/logic/managed_beans/CategoryBrowser.java
+++ BiBi/src/main/java/de/dedede/model/logic/managed_beans/CategoryBrowser.java
@@ -3,16 +3,25 @@ package de.dedede.model.logic.managed_beans;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import de.dedede.model.data.dtos.CategoryDto;
 import de.dedede.model.data.dtos.CategorySearchDto;
 import de.dedede.model.data.dtos.MediumDto;
 import de.dedede.model.persistence.daos.CategoryDao;
 import de.dedede.model.persistence.daos.MediumDao;
+import de.dedede.model.persistence.util.Logger;
 import jakarta.annotation.PostConstruct;
+import jakarta.el.ELContext;
+import jakarta.el.ExpressionFactory;
+import jakarta.el.ValueExpression;
+import jakarta.faces.component.html.HtmlCommandLink;
+import jakarta.faces.component.html.HtmlForm;
 import jakarta.faces.component.html.HtmlOutputText;
 import jakarta.faces.component.html.HtmlPanelGroup;
 import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.el.MethodBinding;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -31,6 +40,9 @@ public class CategoryBrowser extends PaginatedList implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Inject
+	private FacesContext ctx;
+
+	@Inject
 	private ExternalContext ectx;
 
 	@Inject
@@ -40,8 +52,6 @@ public class CategoryBrowser extends PaginatedList implements Serializable {
 
 	private CategoryDto currentCategory;
 
-	// maybe temporary
-	// this is the tree o categories … for now … a flat version of it!!
 	private List<CategoryDto> categories = CategoryDao.readAllCategoriesTemp();;
 
 	private List<MediumDto> mediums;
@@ -49,30 +59,45 @@ public class CategoryBrowser extends PaginatedList implements Serializable {
 	private HtmlPanelGroup categoryTree = new HtmlPanelGroup();
 
 	// initialize the category tree
-	{
-		categoryTree.setLayout("block");
-
-		forEachCategory(categoryTree, categories);
-
-	}
+//	{
+//		categoryTree.setLayout("block");
+//		forEachCategory(categoryTree, categories);
+//	}
 
 	// @Temproary, bad name
-	private static void forEachCategory(HtmlPanelGroup panel, List<CategoryDto> categories) {
-		for (final var subcategory : categories) {
-			final var subpanel = new HtmlPanelGroup();
+	private void forEachCategory(HtmlPanelGroup parentPanel, List<CategoryDto> categories) {
+		for (final var category : categories) {
+			final var panel = new HtmlPanelGroup();
 
 			// @Temp should be a link … kinda commandLink
-			final var text = new HtmlOutputText();
-			text.setValue(subcategory.getName());
+//			final var text = new HtmlOutputText();
+//			text.setValue(subcategory.getName());
+			final var text = new HtmlCommandLink();
+			// @Task setId
+			text.setValue(category.getName());
+			final var elCtx = ctx.getELContext();
+			elCtx.getVariableMapper().setVariable("category", ExpressionFactory.newInstance().createValueExpression(category, CategoryDto.class));
+			text.setActionExpression(ctx.getApplication().getExpressionFactory().createMethodExpression(
+					elCtx, "#{categoryBrowser.selectCategory(category)}", Void.class,
+					new Class<?>[] { CategoryDto.class }));
+			
+			final var form = new HtmlForm();
+			form.getChildren().add(text);
+			
+			panel.setLayout("block");
+			panel.setStyle("margin-left: 3rem;");
+			panel.getChildren().add(form);
 
-			subpanel.setLayout("block");
-			subpanel.setStyle("margin-left: 3rem;");
-			subpanel.getChildren().add(text);
+			parentPanel.getChildren().add(panel);
 
-			panel.getChildren().add(subpanel);
-
-			forEachCategory(subpanel, subcategory.getChildren());
+			forEachCategory(panel, category.getChildren());
 		}
+	}
+	
+	// @Temporary
+	public void build() {
+		categoryTree.setLayout("block");
+		forEachCategory(categoryTree, categories);
 	}
 
 	@PostConstruct
@@ -138,6 +163,8 @@ public class CategoryBrowser extends PaginatedList implements Serializable {
 	}
 
 	public void selectCategory(CategoryDto category) {
+		Logger.development("selectCategory(%s)".formatted(category)); // @Temporary
+		
 		currentCategory = category;
 
 		if (currentCategory != null) {
