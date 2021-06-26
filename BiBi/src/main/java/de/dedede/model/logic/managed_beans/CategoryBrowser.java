4cf815276e2f33c1fdbab4251b5fe6fa6c98c319
León Liehr <liehr@fim.uni-passau.de>
2021-06-26 18:38:31 +0200

    The tree is done and beautiful
    Minus the performance issues since it needs to get rebuild on every page
    load (no view state saving). I am not sure how I can signal that the
    tree does not need to be rebuild, only redrawn?

--- BiBi/src/main/java/de/dedede/model/logic/managed_beans/CategoryBrowser.java
+++ BiBi/src/main/java/de/dedede/model/logic/managed_beans/CategoryBrowser.java
@@ -3,25 +3,20 @@ package de.dedede.model.logic.managed_beans;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
 import de.dedede.model.data.dtos.CategoryDto;
 import de.dedede.model.data.dtos.CategorySearchDto;
 import de.dedede.model.data.dtos.MediumDto;
 import de.dedede.model.persistence.daos.CategoryDao;
 import de.dedede.model.persistence.daos.MediumDao;
-import de.dedede.model.persistence.util.Logger;
 import jakarta.annotation.PostConstruct;
 import jakarta.el.ELContext;
 import jakarta.el.ExpressionFactory;
-import jakarta.el.ValueExpression;
 import jakarta.faces.component.html.HtmlCommandLink;
 import jakarta.faces.component.html.HtmlForm;
-import jakarta.faces.component.html.HtmlOutputText;
 import jakarta.faces.component.html.HtmlPanelGroup;
 import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
-import jakarta.faces.el.MethodBinding;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -30,7 +25,6 @@ import jakarta.inject.Named;
  * Backing bean for the category browser facelet. This page offers users to
  * browse through all available categories mediums are part of in a structured
  * manner. It renders it possible for the consumer to discover new mediums.
- *
  */
 @Named
 @ViewScoped
@@ -45,6 +39,10 @@ public class CategoryBrowser extends PaginatedList implements Serializable {
 	@Inject
 	private ExternalContext ectx;
 
+	private ELContext elctx;
+
+	private ExpressionFactory expr;
+
 	@Inject
 	private UserSession session;
 
@@ -56,58 +54,93 @@ public class CategoryBrowser extends PaginatedList implements Serializable {
 
 	private List<MediumDto> mediums;
 
-	private HtmlPanelGroup categoryTree = new HtmlPanelGroup();
+	private HtmlPanelGroup categoryTree;
 
-	// initialize the category tree
-//	{
-//		categoryTree.setLayout("block");
-//		forEachCategory(categoryTree, categories);
-//	}
+	/**
+	 * The previous category tree category identifier. This field is used as counter
+	 * to get the next identifier.
+	 * 
+	 * The latter is a number uniquely identifying a category inside of the category
+	 * tree.
+	 */
+	private int previousCategoryTreeCategoryId = 0;
 
-	// @Temproary, bad name
-	private void forEachCategory(HtmlPanelGroup parentPanel, List<CategoryDto> categories) {
-		for (final var category : categories) {
-			final var panel = new HtmlPanelGroup();
+	private int previousCategoryTreeId = 0;
 
-			// @Temp should be a link … kinda commandLink
-//			final var text = new HtmlOutputText();
-//			text.setValue(subcategory.getName());
-			final var text = new HtmlCommandLink();
-			// @Task setId
-			text.setValue(category.getName());
-			final var elCtx = ctx.getELContext();
-			elCtx.getVariableMapper().setVariable("category", ExpressionFactory.newInstance().createValueExpression(category, CategoryDto.class));
-			text.setActionExpression(ctx.getApplication().getExpressionFactory().createMethodExpression(
-					elCtx, "#{categoryBrowser.selectCategory(category)}", Void.class,
-					new Class<?>[] { CategoryDto.class }));
-			
-			final var form = new HtmlForm();
-			form.getChildren().add(text);
-			
-			panel.setLayout("block");
-			panel.setStyle("margin-left: 3rem;");
-			panel.getChildren().add(form);
-
-			parentPanel.getChildren().add(panel);
-
-			forEachCategory(panel, category.getChildren());
-		}
-	}
-	
-	// @Temporary
-	public void build() {
-		categoryTree.setLayout("block");
-		forEachCategory(categoryTree, categories);
+	private static HtmlPanelGroup createContainer(String styleClass) {
+		final var component = new HtmlPanelGroup();
+		component.setLayout("block");
+		component.setStyleClass(styleClass);
+		return component;
 	}
 
+	/**
+	 * Initialize this backing bean.
+	 * 
+	 * This method is not meant to be called directly but only by JSF and that only
+	 * once!
+	 */
 	@PostConstruct
 	public void init() {
-
+		expr = ctx.getApplication().getExpressionFactory();
+		elctx = ctx.getELContext();
+		categoryTree = bundleCategories(categories);
 	}
 
-	// @Temporary
-	public String refr() {
-		return "category-browser";
+	private HtmlPanelGroup bundleCategories(List<CategoryDto> categories) {
+
+		final var tree = createContainer("accordion accordion-flush");
+		final var treeId = (previousCategoryTreeId += 1);
+		final var qualifiedTreeId = "category_tree_" + treeId;
+		tree.setId(qualifiedTreeId);
+
+		for (final var category : categories) {
+			final var categoryId = (previousCategoryTreeCategoryId += 1);
+
+			final var categoryNameLink = new HtmlCommandLink();
+			categoryNameLink.setId("link_category_name_" + categoryId);
+			categoryNameLink.setValue(category.getName());
+			elctx.getVariableMapper().setVariable("category", expr.createValueExpression(category, CategoryDto.class));
+			categoryNameLink.setActionExpression(expr.createMethodExpression(elctx,
+					"#{categoryBrowser.selectCategory(category)}", Void.class, new Class<?>[] { CategoryDto.class }));
+			
+			final var qualifiedAccordionCollapseId = "accordion_collapse_" + categoryId;
+			final var accordionButton = new HtmlForm();
+			{
+				final var styleClass = new StringBuilder();
+				styleClass.append("accordion-button collapsed");
+				
+				if (category.getChildren().isEmpty()) {
+					styleClass.append(" disabled");
+				} else {
+					accordionButton.getPassThroughAttributes().put("data-bs-toggle", "collapse");
+				}
+				
+				accordionButton.getChildren().add(categoryNameLink);
+				accordionButton.getPassThroughAttributes().put("data-bs-target", "#" + qualifiedAccordionCollapseId);
+				accordionButton.setStyleClass(styleClass.toString());
+			}
+
+			final var accordionHeader = createContainer("accordion-header");
+			accordionHeader.getChildren().add(accordionButton);
+
+			final var accordionBody = createContainer("accordion-body ps-3 pe-0 py-2");
+
+			final var accordionCollapse = createContainer("accordion-collapse collapse");
+			accordionCollapse.setId(qualifiedAccordionCollapseId);
+			accordionCollapse.getPassThroughAttributes().put("data-bs-parent", "#" + qualifiedTreeId);
+			accordionCollapse.getChildren().add(accordionBody);
+
+			final var accordionItem = createContainer("accordion-item");
+			accordionItem.getChildren().add(accordionHeader);
+			accordionItem.getChildren().add(accordionCollapse);
+
+			tree.getChildren().add(accordionItem);
+
+			accordionBody.getChildren().add(bundleCategories(category.getChildren()));
+		}
+
+		return tree;
 	}
 
 	public CategorySearchDto getCategorySearch() {
@@ -163,8 +196,6 @@ public class CategoryBrowser extends PaginatedList implements Serializable {
 	}
 
 	public void selectCategory(CategoryDto category) {
-		Logger.development("selectCategory(%s)".formatted(category)); // @Temporary
-		
 		currentCategory = category;
 
 		if (currentCategory != null) {
