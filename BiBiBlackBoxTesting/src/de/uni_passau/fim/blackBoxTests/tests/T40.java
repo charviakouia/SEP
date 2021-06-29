b93f80fbffde26acbdfb5ab3c4cbfae9513cfd2b
Sergei Pravdin <sergpravdin@icloud.com>
2021-06-29 23:44:40 +0200

    T40 und IDs in facelets

---
+++ BiBiBlackBoxTesting/src/de/uni_passau/fim/blackBoxTests/tests/T40.java
@@ -0,0 +1,98 @@
+package de.uni_passau.fim.blackBoxTests.tests;
+
+import de.uni_passau.fim.blackBoxTests.test_suite.Driver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.*;
+
+public class T40 {
+	
+	private WebDriver driver;
+	private WebDriverWait waiter;
+	private String threadName;
+	private boolean isMultiThreaded = false;
+		    
+    @Before
+    public void setUp() {
+    	if (!isMultiThreaded) {
+    		driver = Driver.getDriver();
+    		waiter = Driver.getDriverWait();
+    	}
+    }
+    
+    @Test
+    public void doTest() {
+        try {
+			//navigates to medium creator
+			waiter.until(ExpectedConditions.visibilityOfElementLocated(By.id("header_staff_dropdown")));
+			waiter.until(ExpectedConditions.visibilityOfElementLocated(By.id("header_staff_dropdown"))).click();
+			waiter.until(ExpectedConditions.visibilityOfElementLocated(By.id("medium-creator")));
+			waiter.until(ExpectedConditions.visibilityOfElementLocated(By.id("medium-creator"))).click();
+
+			//sets a medium's attributes
+			driver.findElement(By.id("mediumForm:mediumTitle")).sendKeys("Programmieren lernen");
+			driver.findElement(By.id("mediumForm:author1")).sendKeys("Mustermann");
+			driver.findElement(By.id("mediumForm:mediumType")).sendKeys("Buch");
+			driver.findElement(By.id("mediumForm:mediumEdition")).sendKeys("1.0");
+			driver.findElement(By.id("mediumForm:mediumPublisher")).sendKeys("Springer");
+			driver.findElement(By.id("mediumForm:releaseYear")).clear();
+			driver.findElement(By.id("mediumForm:releaseYear")).sendKeys("2020");
+			driver.findElement(By.id("mediumForm:releaseYear")).sendKeys("2020");
+			driver.findElement(By.id("mediumForm:isbn")).sendKeys("17RE");
+			driver.findElement(By.id("mediumForm:copyLocation")).sendKeys("FIM");
+			driver.findElement(By.id("mediumForm:copySignature")).sendKeys("17RE (1)");
+
+			//create
+			waiter.until(ExpectedConditions.visibilityOfElementLocated(By.id("mediumForm:medium-create-btn")));
+			waiter.until(ExpectedConditions.visibilityOfElementLocated(By.id("mediumForm:medium-create-btn"))).click();
+
+			//checks a result
+			assertTrue(driver.getPageSource().contains("Medium erfolgreich erstellt"));
+        } catch (Exception e) {
+        	fail("A positive message not found.");
+        }
+    }
+    
+    @After
+    public void tearDown() {}
+    
+    public WebDriver getDriver() {
+		return driver;
+	}
+    
+	public boolean isMultiThreaded() {
+		return isMultiThreaded;
+	}
+
+	public void setMultiThreaded(boolean isMultiThreaded) {
+		this.isMultiThreaded = isMultiThreaded;
+	}
+
+
+	public void setDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public WebDriverWait getWaiter() {
+		return waiter;
+	}
+
+	public void setWaiter(WebDriverWait waiter) {
+		this.waiter = waiter;
+	}
+
+	public String getThreadName() {
+		return threadName;
+	}
+
+	public void setThreadName(String threadName) {
+		this.threadName = threadName;
+	}
+}
