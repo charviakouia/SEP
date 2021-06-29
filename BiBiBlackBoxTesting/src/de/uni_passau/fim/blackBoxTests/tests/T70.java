b726ba9e715ca195d5802106646affeec8e899df
Sergei Pravdin <sergpravdin@icloud.com>
2021-06-30 01:56:25 +0200

    T70 und id

--- BiBiBlackBoxTesting/src/de/uni_passau/fim/blackBoxTests/tests/T70.java
+++ BiBiBlackBoxTesting/src/de/uni_passau/fim/blackBoxTests/tests/T70.java
@@ -5,10 +5,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static de.uni_passau.fim.blackBoxTests.test_suite.UrlPrefix.BASE_URL;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -29,10 +31,26 @@ public class T70 {
     
     @Test
     public void doTest() {
-		//test is coming soon
+		//search
+		driver.findElement(By.id("form_medium_search_header:input_medium_search_term_header"))
+				.sendKeys("Programmieren lernen" + Keys.ENTER);
+
+		//navigate
+		waiter.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[id$=mediumLink]")));
+		waiter.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[id$=mediumLink]"))).click();
+
+		//edit a category of the medium
+		driver.findElement(By.id("form_mediumAttributes_forUsers:category")).clear();
+		driver.findElement(By.id("form_mediumAttributes_forUsers:category"))
+				.sendKeys("Informatik");
+
+		//save
+		waiter.until(ExpectedConditions.visibilityOfElementLocated(By.id("form_mediumAttributes_forUsers:saveMedium")));
+		waiter.until(ExpectedConditions.visibilityOfElementLocated(By.id("form_mediumAttributes_forUsers:saveMedium"))).click();
 
         try {
 			//checks a result
+			assertTrue(driver.getPageSource().contains("Das Medium ist erfolgreich geändert."));
 
         } catch (Exception e) {
         	fail("A positive message not found.");
