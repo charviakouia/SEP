7fb440e9a692ac57b9cfc3cb4193adf22fadce04
Jonas Picker <picker@fim.uni-passau.de>
2021-06-30 15:16:40 +0200

    smol pp

--- BiBiBlackBoxTesting/src/de/uni_passau/fim/blackBoxTests/tests/TestSuiteThread.java
+++ BiBiBlackBoxTesting/src/de/uni_passau/fim/blackBoxTests/tests/TestSuiteThread.java
@@ -16,6 +16,7 @@ public class TestSuiteThread extends Thread {
 	
 	@Override
 	public void run() {
+		
 		T01 test1 = new T01();
 		T02 test2 = new T02();
 		test1.setDriver(suiteDriver);
@@ -24,22 +25,29 @@ public class TestSuiteThread extends Thread {
 		test2.setWaiter(suiteWaiter);
 		test1.setMultiThreaded(true);
 		test2.setMultiThreaded(true);
-		test1.setThreadName(Thread.currentThread().getName());
-		test2.setThreadName(Thread.currentThread().getName());
+		String threadName = Thread.currentThread().getName();
+		test1.setThreadName(threadName);
+		test2.setThreadName(threadName);
 		test1.setUp();
 		test2.setUp();
 		try {
 			Thread.sleep(1000);
+			long starttime = System.currentTimeMillis();
+			System.out.println("TestSuite Thread with name: " + threadName + "started executing at: " + starttime);
+			
 			test1.doTest();
-			Thread.sleep(1000);
 			test2.doTest();
+			
+			long stoptime = System.currentTimeMillis();
+			System.out.println("Finished executing test suite with thread name: " + threadName + " at time: " + stoptime);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+			System.out.println("Thread with name: " + threadName + " was interrupted from its sleep.");
 		} finally {
-			System.out.println("Closing web driver for suite with thread name: " + Thread.currentThread().getName());
+			System.out.println("Closing web driver for suite with thread name: " + threadName);
 			suiteDriver.close();
 		}
-		System.out.println("Finished executing test suite with thread name: " + Thread.currentThread().getName());
+		
 		
 	}
 
